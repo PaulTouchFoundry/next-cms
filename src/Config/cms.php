8d009814a1be3f26abab448c120f04ea410f7b8c
bagf <rory@eishgaming.co.za>
2016-11-15 17:58:00 +0200

    Added default config files

---
+++ src/Config/cms.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'group' => [
+        'prefix' => 'admin',
+    ],
+    'brand' => [
+        'color' => '#737373',
+        'title' => 'Wearenext CMS',
+    ],
+    'features' => [
+        'page_hero' => Wearenext\CMS\Models\Features\Hero::class,
+        'page_products' => Wearenext\CMS\Models\Features\Product::class,
+    ],
+    'auth' => [
+        'middleware' => 'auth',
+    ],
+    /**
+     * @todo CMS icon set issue #1
+     */
+    'icon_set' => 'default',
+    'icons' => [
+        'prefix' => [
+            'default' => '',
+        ],
+        'default' => [],
+    ],
+];
---
+++ src/Config/services.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'cloudinary' => [
+        'cloud_name' => 'du9dtwhdr',
+        'api_key' => '217985456743237',
+        'api_secret' => 'lVjdxOIzHKZVbmuPZsbmG_NYIq4',
+    ],
+];
