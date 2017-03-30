b903e509e8cad881ead694163bd7179db0a89c26
Warra <warren@wearenext.co.za>
2017-03-30 10:43:29 +0200

    Creates fund document page.

---
+++ src/Database/Migrations/2017_03_30_083627_create_fund_document_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFundDocumentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fund_document', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('route');
+            $table->string('product_name');
+            $table->string('file_path');
+            $table->string('file_name');
+            $table->string('file_size')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fund_document');
+    }
+}
